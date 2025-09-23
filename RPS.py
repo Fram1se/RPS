b0abe6a2e2617d9db4792b2b9f36a66b1963c6db
Fram1se <bebebe@example.com>
2025-09-23 16:30:32 +0300

    pythonproject

---
+++ RPS.py
@@ -0,0 +1,8 @@
+number = float(input("Впиши число: "))
+
+if number > 5:
+    print("Ты крут!")
+elif number < 5:
+    print("Ты не крут :(")
+else:
+    print("Ты ровно на грани... 5 — это нейтрально!")
