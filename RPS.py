117b0cfa7cea492b3ec351abfe611b1da346dc22
Fram1se <malexd.888@gmail.com>
2025-09-24 12:07:42 +0300

    newpythonproj

--- RPS.py
+++ RPS.py
@@ -2,12 +2,12 @@ import random
 
 def guess_number():
     print("Игра 'Угадай число'!")
-    number = random.randint(1, 100)
+    number = random.randint(1, 50)
     attempts = 0
     
     while True:
         try:
-            guess = int(input("Угадайте число от 1 до 100: "))
+            guess = int(input("Угадайте число от 1 до 50: "))
             attempts += 1
             
             if guess < number:
